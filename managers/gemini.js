113bd5dc93593ba0bcec1cf2dadf208dacec4c32
Ennio Marke <mystical.ennio@gmail.com>
2024-10-30 15:27:14 +0100

    v2.0

--- managers/gemini.js
+++ managers/gemini.js
@@ -1,5 +1,12 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
-import db from "../responses.sqlite" with { "type": "sqlite" };
+import { Database } from 'bun:sqlite';
+import log from './logger.js';
+
+const db = new Database('responses.sqlite');
+
+db.query(
+    'CREATE TABLE IF NOT EXISTS responses (prompt TEXT PRIMARY KEY, response TEXT)'
+).run();
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
 
@@ -7,36 +14,49 @@ const model = genAI.getGenerativeModel({
     model: 'gemini-1.5-flash',
     systemInstruction: await Bun.file('system-instruction.txt').text(),
     generationConfig: {
-        responseMimeType: "application/json",
+        responseMimeType: 'application/json',
         responseSchema: await Bun.file('response-schema.json').json(),
-      },
+    },
 });
 
 const chat = model.startChat();
 
-db.exec("PRAGMA journal_mode = WAL;");
+db.exec('PRAGMA journal_mode = WAL;');
 
 export default (app) => {
-    app.get("*", async (req, res) => {
-        //GET /api/path/to route
-        const prompt = `${req.method} ${req.url}`;
-
+    app.get('*', async (req, res) => {
         try {
-            const cached = db.query('SELECT response FROM responses WHERE prompt = ?', [prompt]).get()
+            //GET /api/path/to route
+            const prompt = `${req.method} ${req.url}`;
+
+            const cached = db
+                .query('SELECT response FROM responses WHERE prompt = $prompt')
+                .get({ $prompt: prompt });
             if (cached) {
-                res.status(200).json(cached);
-                return;
+                log(`Loaded cached response for ${prompt}`);
+                log(cached);
+                return res.status(200).json(JSON.parse(cached.response));
             }
+            const result = await chat.sendMessage(prompt).catch((e) => {
+                log('error', e.toString());
+                return res.status(500).json({ error: e.toString() });
+            });
+            const responseText = await result.response?.text();
+            log(responseText);
+            if (!responseText) {
+                return res.status(500).json({ error: 'No response' });
+            }
+            const response = await JSON.parse(responseText);
+            if (!response.cache_response) return;
+            db.query(
+                'INSERT INTO responses (prompt, response) VALUES ($prompt, $response)'
+            ).run({ $prompt: prompt, $response: JSON.stringify(response) });
+            log(`Cached response for ${prompt}`);
 
-            const result = await chat.sendMessage(prompt);
-            const response = JSON.parse((await result.response).text());
-            res.status(200).json(response);
-
-            if(!response.cache_response) return;
-            db.query('INSERT INTO responses (prompt, response) VALUES ($prompt, $response)').run({$prompt: prompt, $response: response});
-            console.log(`Cached response for ${prompt}`);
+            return res.status(200).json(response);
         } catch (e) {
-            return res.status(500).json(e);
+            log('error', e.toString());
+            return res.status(500).json({ error: e.toString() });
         }
-    })
+    });
 };
